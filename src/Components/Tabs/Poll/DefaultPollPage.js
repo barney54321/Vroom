fa9d9a4d0d779911dc52b01accb0272433f2675e
Lilian Hunt <lilianhunt@Lilians-Air.modem>
2021-08-27 22:37:38 +1000

    poll page

--- src/Components/Tabs/Poll/DefaultPollPage.js
+++ src/Components/Tabs/Poll/DefaultPollPage.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import ExistingPoll from './ExistingPoll'
 import { ListGroup } from 'react-bootstrap'
+import {useState} from 'react'
+import Button from 'react-bootstrap/Button'
+
 const DefaultPollPage = (props) => {
-    // const polls = [{id: 1, description: "Poll 1"}]
-    const polls = props.polls;
-    
+    const [polls, setPolls] = useState([{id: 0, name: "Poll 1"},{id: 1, name: "Poll 2"}, {id: 2, name: "Poll 3"} ])
+
+
     const deletePoll = (index) => {
         const copy = [...polls];
         copy.splice(index, 1);
-        props.setPolls(copy);
+        setPolls(copy);
     }
 
+    const addPoll = () => {
+        console.log("add poll clicked");
+    }
     return (
         <div>
+            <h4>Existing Polls</h4>
             <ListGroup defaultActiveKey="">
 
-            {polls.map((poll) => (<ExistingPoll key={polls.id} poll={poll}/>))}
+            {polls.map((poll, index) => (<ExistingPoll key={poll.id} poll={poll} index={index} deletePoll={deletePoll}/>))}
         
             </ListGroup>
+            <Button variant="primary" onClick={addPoll}>Add Poll</Button>
         </div>
     )
 }
--- src/Components/Tabs/Poll/ExistingPoll.js
+++ src/Components/Tabs/Poll/ExistingPoll.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap';
-
+import CloseButton from 'react-bootstrap/CloseButton'
 const ExistingPoll = (props) => {
     const poll = props.poll;
+  
     return (
         <div>
-            <ListGroup.Item action href={poll.link}>{poll.name}</ListGroup.Item>
+            <ListGroup.Item action href={poll.link}>{poll.name} <CloseButton onClick={() => props.deletePoll(props.index)}></CloseButton></ListGroup.Item>
         </div>
     )
 }
