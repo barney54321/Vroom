2669814f77e6d5b0aba1ec35f12a46748b78b9a1
Lilian Hunt <lilianhunt@Lilians-Air.modem>
2021-08-28 00:05:50 +1000

    create lesson plan view

--- src/Components/Common/VroomContext.js
+++ src/Components/Common/VroomContext.js
@@ -8,6 +8,7 @@ export const VroomContextProvider = (props) => {
     const [showPoll, setShowPoll] = useState(false);
     const [currentPoll, setCurrentPoll] = useState("");
     const [polls, setPolls] = useState([{name: "Poll 1"},{name: "Poll 2"}, {name: "Poll 3"} ])
+    const [lessonPlan, setLessonPlan] = useState({title: "Week 1", contents: [{name: "Java intro", description: "teachers notes", time:"10"}]})
 
     return (
         <VroomContext.Provider
@@ -20,6 +21,8 @@ export const VroomContextProvider = (props) => {
                 setShowPoll,
                 currentPoll,
                 setCurrentPoll,
+                lessonPlan,
+                setLessonPlan,
             }}
         >
             {props.children}
