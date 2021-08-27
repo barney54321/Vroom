b683af7ed9d6bc9036722cb8e9bea9e886c1be9d
Andrew Esteban <andrewesteban1999@gmail.com>
2021-08-27 20:39:07 +1000

    added join endpoint

--- backend/backend.js
+++ backend/backend.js
@@ -12,6 +12,19 @@ app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
+/*
+    JOIN - POST
+    Send JSON body with url and name: {url: "zoom.com", name: "Teaching Assistant"}
+    Returns 200 on success, 400 on failure
+*/
+app.post("/join", (req, res) => {
+    let body = req.body;
+    let url = body.url;
+    let name = body.name;
+    console.log(url + " " + name);
+    res.sendStatus(200);
+});
+
 app.listen(port, () => {
     console.log("Listening on port " + port);
 });
