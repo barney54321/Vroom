cc6ea886a51db2f9a593499874413f4da2b1048d
Samantha Millett <smil5953@uni.sydney.edu.au>
2021-08-27 21:26:57 +1000

    poll

---
+++ src/Components/Common/PercentageBar.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import Card from 'react-bootstrap/Card';
+
+
+const PercentageBar = (props) => {
+
+    useEffect(() => {
+        document.getElementById(props.index).style.width = props.value + "%";
+    })
+
+    const activeClass = props.active === true ? " active" : "";
+
+    return (
+        <Card className="poll-result" border={"primary"} onClick={() => props.handleClick(props.index)}>
+            <div className={"color-result"} id={props.index}>
+                <Card.Body className={"poll-result-text" + activeClass }>{`${props.value}% ${props.name}`}</Card.Body>
+            </div>
+        </Card>
+    );
+}
+
+export default PercentageBar;
