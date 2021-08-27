15fcb145ed0669711277d043a4fc54d518caf7fe
Lilian Hunt <lilianhunt@Lilians-Air.modem>
2021-08-28 01:46:42 +1000

    create poll not launched

---
+++ src/Components/Common/Bar.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import Card from 'react-bootstrap/Card';
+
+
+const Bar = (props) => {
+
+
+    return (
+        <Card className="poll-result" border={"primary"}>
+            <div className={"color-result"} id={`x ${props.index}`}>
+                <Card.Body className="poll-bar">{props.name}</Card.Body>
+            </div>
+        </Card>
+    );
+}
+
+export default Bar;
