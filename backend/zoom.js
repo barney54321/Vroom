bae1d83b6f389d269e34f32164f3453381eafa55
Andrew Esteban <andrewesteban1999@gmail.com>
2021-08-27 22:38:45 +1000

    joined zoom call

--- backend/zoom.js
+++ backend/zoom.js
@@ -1,9 +1,51 @@
-class Zoom {
+require("chromedriver");
+const webdriver = require("selenium-webdriver");
+const { By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+class Zoom {
     async init(link, name) {
-        this.link = link;
+        this.link = link.replace("/j/", "/wc/join/");
         this.name = name;
-        console.log("Joined meeting " + link + " with name " + name);
+
+        var options = new chrome.Options();
+        options.addArguments("--mute-audio");
+        // options.addArguments("--headless");
+
+        var builder = new webdriver.Builder();
+        builder.forBrowser("chrome");
+        builder.setChromeOptions(options);
+
+        this.driver = builder.build();
+
+        // Go to join page
+        await this.driver.get(this.link);
+
+        // Fill in name input field
+        let nameField = await this.driver.findElement(By.id("inputname"));
+        await nameField.sendKeys("Vroom");
+
+        // Wait for button to be pressable
+        await sleep(1000);
+
+        // Press button (obscured by annoying div so using js)
+        await this.driver.executeScript("document.getElementById('joinBtn').click()");
+
+        // Get the chat button
+        let chatButton = await this.driver.findElement(webdriver.By.className("footer-button__chat-icon"));
+
+        // Wait a bit more for everything to load
+        await sleep(5000);
+
+        await chatButton.click();
+
+        console.log("Joined meeting " + this.link + " with name " + name);
     }
 
     async leave() {
@@ -20,23 +62,23 @@ class Zoom {
             questions: [
                 {
                     question: 1,
-                    names: [
-                        "Name 1",
-                        "Name 2"
-                    ]
+                    names: ["Name 1", "Name 2"],
                 },
                 {
                     question: 2,
-                    names: [
-                        "Name 3"
-                    ]
-                }
-            ]
+                    names: ["Name 3"],
+                },
+            ],
         };
     }
 
     async launchPoll(question, options) {
-        console.log("Launching poll with question " + question + "and options " + options);
+        console.log(
+            "Launching poll with question " +
+                question +
+                "and options " +
+                options
+        );
     }
 
     async getResults() {
@@ -44,18 +86,13 @@ class Zoom {
             options: [
                 {
                     option: "True",
-                    names: [
-                        "Student A",
-                        "Student B"
-                    ]
+                    names: ["Student A", "Student B"],
                 },
                 {
                     option: "False",
-                    names: [
-                        "Student C"
-                    ]
-                }
-            ]
+                    names: ["Student C"],
+                },
+            ],
         };
     }
 
@@ -64,24 +101,19 @@ class Zoom {
             options: [
                 {
                     option: "True",
-                    names: [
-                        "Student A",
-                        "Student B"
-                    ]
+                    names: ["Student A", "Student B"],
                 },
                 {
                     option: "False",
-                    names: [
-                        "Student C"
-                    ]
-                }
-            ]
+                    names: ["Student C"],
+                },
+            ],
         };
     }
 
     async startPlan(plan) {
         console.log("Plan started: " + plan);
-    } 
+    }
 
     async stopPlan() {
         console.log("Plan stopped");
@@ -92,4 +124,11 @@ class Zoom {
     }
 }
 
-module.exports = {Zoom};
+module.exports = { Zoom };
+
+async function test() {
+    let zoom = new Zoom();
+    await zoom.init("https://uni-sydney.zoom.us/j/83168226455", "Vroom");
+}
+
+test();
