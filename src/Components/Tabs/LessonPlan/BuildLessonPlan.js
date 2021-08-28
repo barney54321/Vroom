916730520079a37e8291a5066c611b0db3f71459
Rhiannon Hames <rhiannon@hames.info>
2021-08-28 12:57:22 +1000

    changes pre lunch

--- src/Components/Tabs/LessonPlan/BuildLessonPlan.js
+++ src/Components/Tabs/LessonPlan/BuildLessonPlan.js
@@ -115,10 +115,9 @@ const BuildLessonPlan = (props) => {
                 <div className="d-flex align-items-start w-100">
                 <Button variant="outline-primary" onClick={handleAdd}>+ SECTION</Button>
                 </div>
-                
-                <div className="your-meeting-button">
+            </div>
+            <div className="d-flex justify-content-end align-items-center your-meeting-button">
                     <Button onClick={handleSave}>Save</Button>
-                </div>
             </div>
         </div>
 
